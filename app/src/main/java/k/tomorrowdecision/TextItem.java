ec381d3644857d0cb06507aae7cc18084972ed0a
kodong <kodonghyun1209@gmail.com>
2016-12-03 19:42:08 +0900

    Memorize 및 Must do 아이템을 보여주기 위한 설정
     - 리스트 아이템 xml 파일 추가
     - TextItem 클래스 생성
     - Text 리스트 뷰 어댑터 추가

---
+++ app/src/main/java/k/tomorrowdecision/TextItem.java
@@ -0,0 +1,22 @@
+package k.tomorrowdecision;
+
+public class TextItem {
+    private int id;
+    private String todo;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTodo() {
+        return todo;
+    }
+
+    public void setTodo(String todo) {
+        this.todo = todo;
+    }
+}
