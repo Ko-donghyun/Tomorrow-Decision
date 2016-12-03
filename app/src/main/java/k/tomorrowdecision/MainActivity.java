456253e126b79c766d0004875c6aab5bb5777bbc
kodong <kodonghyun1209@gmail.com>
2016-12-03 19:43:39 +0900

    필요한 변수 생성

--- app/src/main/java/k/tomorrowdecision/MainActivity.java
+++ app/src/main/java/k/tomorrowdecision/MainActivity.java
@@ -1,27 +1,86 @@
 package k.tomorrowdecision;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.animation.AnimationUtils;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ViewFlipper;
 
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+    InputMethodManager imm;
+    // 할 일 디비
+    TodoDataBase todoDataBase;
+    final String todoDBName = "Todo.db";
+    final int todoDBVersion = 1;
+    MemorizeDataBase memorizeDataBase;
+    final String memorizeDBName = "memorize.db";
+    final int memorizeDBVersion = 1;
+    MustDoDataBase mustDoDataBase;
+    final String mustDoDBName = "mustDo.db";
+    final int mustDoDBVersion = 1;
 
     ViewFlipper mainViewFlipper;
 
     ListView memorizeListView;
-
     ListView mustDoListView;
-
     ListView todoListView;
-    TodoListViewAdapter todoListViewAdapter;
 
+    TodoListViewAdapter todoListViewAdapter;
+    TextListViewAdapter mustDoListViewAdapter;
+    TextListViewAdapter memorizeListViewAdapter;
+
+    String setTextColor = "#000000";
+    String setBackgroundColor = "#FFFFFF";
+    String timeText = "";
+    int itemPosition;
+
+    Boolean clickFlag = true;
+    Button cancelButton;
+    Button doneButton;
+
+    private final long FINISH_INTERVAL_TIME = 1000;
+    private long backPressedTime = 0;
+
+    TextView todoTime;
+    EditText todo;
+    LinearLayout todoBackground;
+
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM월 dd일 (E) HH시 mm:ss", Locale.getDefault());
+
+    int[] buttonArray = {
+      R.id.color_picker_01, R.id.color_picker_02, R.id.color_picker_03,
+      R.id.color_picker_04, R.id.color_picker_05, R.id.color_picker_06,
+      R.id.color_picker_07, R.id.color_picker_08, R.id.color_picker_09,
+    };
+
+    Button backgroundColorPicker;
+    Button textColorPicker;
+    String colorPickerSwitch = "text";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
