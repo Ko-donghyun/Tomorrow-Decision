8482fc49344194901a77816ef62c0ed1191d7e94
kodong <kodonghyun1209@gmail.com>
2016-12-27 23:50:01 +0900

    할일 저장 제한 다이얼로그 수정
    나의 시간대를 저장할 수 있게됨으로서
    할 일 저장 시간을 유동적으로 보여줘야 함에 따른 처리

--- app/src/main/java/k/tomorrowdecision/Dialog/InformationDialog.java
+++ app/src/main/java/k/tomorrowdecision/Dialog/InformationDialog.java
@@ -6,12 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.TextView;
 
 import k.tomorrowdecision.R;
 
 public class InformationDialog extends Dialog {
 
     private View.OnClickListener okayClickListener;
+    private String editableTimeRange;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +26,17 @@ public class InformationDialog extends Dialog {
         getWindow().setAttributes(lpWindow);
         setContentView(R.layout.information_dialog);
 
+        TextView editableTime = (TextView) findViewById(R.id.editable_time);
+        editableTime.setText(editableTimeRange);
+
         Button cancelButton = (Button) findViewById(R.id.dialog_okay);
         cancelButton.setOnClickListener(okayClickListener);
     }
 
-    public InformationDialog(Context context, View.OnClickListener okayClickListener) {
+    public InformationDialog(Context context, View.OnClickListener okayClickListener, String editableTimeRange) {
         super(context, android.R.style.Theme_Translucent_NoTitleBar);
         this.okayClickListener = okayClickListener;
-
+        this.editableTimeRange = editableTimeRange;
     }
 
 
